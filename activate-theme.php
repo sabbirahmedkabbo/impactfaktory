af3af3b83515e5dd8832a542117d5f52b1af97f2
sabbirahmedkabbo <sabbirahmed.kabbo@gmail.com>
2026-06-30 21:46:13 +0600

    Auto-update: Sync modifications

--- activate-theme.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/wp-load.php';
-echo "Current theme before: " . wp_get_theme()->get('Name') . "\n";
-echo "Switching theme to impact-faktory...\n";
-switch_theme('impact-faktory');
-echo "Current theme after: " . wp_get_theme()->get('Name') . "\n";
